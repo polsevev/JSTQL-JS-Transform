38fe05af7f99cbcc2f9cf1cba025127eb033c645
polsevev <rolf.martin@glomsrud.no>
2023-10-11 16:28:24 +0200

    LESSGOOOO Finally i added some stupid custom syntax :)

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
 import * as babelparser from "../babel/packages/babel-parser/lib";
-import traverse from "@babel/traverse";
-import generate from "@babel/generator";
+import traverse from "../babel/packages/babel-traverse/lib";
+import generate from "../babel/packages/babel-generator/lib";
 
 const main = () => {
-  
-  let code_To_Insert = "697 + 457";
+    let code_To_Insert = "697 + 457";
 
-  let code = "1 + 1;";
-  let ast = babelparser.parse(code);
-  console.log(ast); 
-  let insert_ast = babelparser.parse(code);
+    let code = "1 + 1;";
+    let ast = babelparser.parse(code);
+    console.log(ast);
+    let insert_ast = babelparser.parse(code);
 
+    traverse(ast, {
+        enter(path) {
+            if (path.isBinaryExpression({ operator: "+" })) {
+                path.node.operator = "@@@";
+            }
+        },
+    });
 
-  traverse(ast, {
-    enter(path){
-      if (path.isBinaryExpression({operator: "+"})){
-        path.node.operator="@@@";
-      }
-    },
-  })
- 
-  console.log(JSON.stringify(ast, null, 4));
-  const out = generate(ast, {}, code);
-  console.log("input: " +  code);
-  console.log("output: " +  out.code);
-  let inout = babelparser.parse(out.code);
-  console.log(inout);
-  console.log(generate(inout, {}, code));
-  
-
-}
-
+    console.log(JSON.stringify(ast, null, 4));
+    const out = generate(ast, {}, code);
+    console.log("input: " + code);
+    console.log("output: " + out.code);
+    let inout = babelparser.parse(out.code);
+    console.log(inout);
+    console.log(generate(inout, {}, code));
+};
 
 main();
