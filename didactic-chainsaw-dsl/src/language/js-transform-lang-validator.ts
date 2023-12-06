44ce0e69fcac405a29ab2672a1fc15048e794e14
polsevev <rolf.martin@glomsrud.no>
2023-12-06 13:44:49 +0100

    Initialized Langium, i am however having some issues with it, perhaps writing my own parser is better?

---
+++ didactic-chainsaw-dsl/src/language/js-transform-lang-validator.ts
@@ -0,0 +1,34 @@
+import type { ValidationAcceptor, ValidationChecks } from "langium";
+import type { JsTransformLangAstType, Proposal } from "./generated/ast.js";
+import type { JsTransformLangServices } from "./js-transform-lang-module.js";
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: JsTransformLangServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.JsTransformLangValidator;
+    const checks: ValidationChecks<JsTransformLangAstType> = {
+        Proposal: validator.checkPersonStartsWithCapital,
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class JsTransformLangValidator {
+    checkPersonStartsWithCapital(
+        proposal: Proposal,
+        accept: ValidationAcceptor
+    ): void {
+        if (proposal.code) {
+            if (proposal.code === "") {
+                accept("warning", "You are running with empty code here", {
+                    node: proposal,
+                    property: "code",
+                });
+            }
+        }
+    }
+}
