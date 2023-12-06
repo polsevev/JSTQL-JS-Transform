163240b9b20b57e0207ec961296667f634cbfe5f
polsevev <rolf.martin@glomsrud.no>
2023-12-06 18:48:30 +0100

    fixed babel submodule setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 import * as babelparser from "../babel/packages/babel-parser";
 
-const main = (): void => {};
+/*
+proposal await_to_promise {
+    applicable to {
+        let a = await b();
+        console.log(b);
+    }
+
+    transform to {
+        b().then((a) => {
+            console.log(a);
+        })
+    }
+
+}
+
+
+*/
+
+
+const main = (
+
+): void => {};
 
 main();
