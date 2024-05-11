ae3246a53ac75c4e889c772185b0429fef4fd27f
polsevev <rolf.martin@glomsrud.no>
2024-05-11 16:09:03 +0200

    Finished up Langium stuffs and more more more

--- JSTQL/src/cli/main.ts
+++ JSTQL/src/cli/main.ts
@@ -1,8 +1,8 @@
 import type { Model } from '../language/generated/ast.js';
 import chalk from 'chalk';
 import { Command } from 'commander';
-import { JsTransformLangLanguageMetaData } from '../language/generated/module.js';
-import { createJsTransformLangServices } from '../language/js-transform-lang-module.js';
+import { JstqlLanguageMetaData } from '../language/generated/module.js';
+import { createJstqlServices } from '../language/jstql-module.js';
 import { extractAstNode } from './cli-util.js';
 import { generateJavaScript } from './generator.js';
 import { NodeFileSystem } from 'langium/node';
@@ -15,7 +15,7 @@ const packagePath = path.resolve(__dirname, '..', '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
-    const services = createJsTransformLangServices(NodeFileSystem).JsTransformLang;
+    const services = createJstqlServices(NodeFileSystem).Jstql;
     const model = await extractAstNode<Model>(fileName, services);
     const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
     console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
@@ -30,7 +30,7 @@ export default function(): void {
 
     program.version(JSON.parse(packageContent).version);
 
-    const fileExtensions = JsTransformLangLanguageMetaData.fileExtensions.join(', ');
+    const fileExtensions = JstqlLanguageMetaData.fileExtensions.join(', ');
     program
         .command('generate')
         .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
--- src/matcher/matcher.ts
+++ src/matcher/matcher.ts
@@ -19,138 +19,147 @@ export interface PairedNodes {
 export function runMatch(
     code: TreeNode<t.Node>,
     applicableTo: TreeNode<t.Node>,
-    internals: Map<string, InternalDSLVariable>
+    internals: InternalDSLVariable
 ): TreeNode<PairedNodes>[] {
-    let matches: TreeNode<t.Node>[] = [];
-
-    function checkDSLInternals(code_node: t.Node, aplTo: t.Node): boolean {
-        if (aplTo.type == "Identifier" && internals.has(aplTo.name)) {
-            let dsl_types = internals.get(aplTo.name)?.type ?? [];
-
-            for (const dsl_type of dsl_types) {
-                if (dsl_type == "Expression") {
-                    if (
-                        code_node.type.includes("Expression") ||
-                        code_node.type == "StringLiteral"
-                    ) {
-                        return true;
-                    }
-                } else if (dsl_type == "") {
-                    if (
-                        code_node.type == "Identifier" &&
-                        aplTo.type == "Identifier"
-                    ) {
-                        return true;
-                    }
-                } else if (dsl_type == "Identifier") {
-                    if (code_node.type == "Identifier") {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+    // Special case for a single expression, we have to remove "ExpressionStatement" node.
+    if (
+        applicableTo.children.length === 1 &&
+        applicableTo.children[0].element.type === "ExpressionStatement"
+    ) {
+        let matcher = new Matcher(
+            internals,
+            applicableTo.children[0].children[0].element
+        );
+        matcher.singleExprMatcher(code, applicableTo.children[0].children[0]);
+        return matcher.matches;
+    } else {
+        let matcher = new Matcher(internals, applicableTo.element);
+        matcher.multiStatementMatcher(code, applicableTo);
+        return matcher.matches;
     }
-
-    function match(
-        code: TreeNode<t.Node>,
-        applicableTo: TreeNode<t.Node>
-    ): boolean {
-        if (code.element.type == "Program") {
-            code.children.forEach((code_child) => {
-                match(code_child, applicableTo);
-            });
-        }
-
-        // This is a bit wierd, as we currently do not support having ApplicableTo be multiple statements
-        if (applicableTo.element.type == "Program") {
-            match(code, applicableTo.children[0]);
-        }
-
-        let node_matches = checkCodeNode(code.element, applicableTo.element);
-
-        //If element matches DSL internals, we return right away and ignore the contents
-        if (checkDSLInternals(code.element, applicableTo.element)) {
-            return true;
-        }
-
-        if (node_matches) {
-            if (applicableTo.children.length != code.children.length) {
-                return false;
-            }
-            for (let i = 0; i < applicableTo.children.length; i++) {
-                //Verify we can actually do a match
-                if (!match(code.children[i], applicableTo.children[i])) {
-                    return false;
-                }
-            }
-            return true;
-        } else {
-            for (let code_child of code.children) {
-                //Avoid matching on single identifier
-                if (code_child.element.type == "Identifier") {
-                    continue;
-                }
-                if (match(code_child, applicableTo)) {
-                    matches.push(code_child);
-                }
-            }
-            return false;
-        }
-    }
-    match(code, applicableTo);
-    console.log(matches.length);
-
-    return matches
-        .map((match) => pairMatch(match, applicableTo))
-        .filter((match) => match != null);
 }
 
-function pairMatch(
-    match: TreeNode<t.Node>,
-    aplTo: TreeNode<t.Node>
-): TreeNode<PairedNodes> | null {
-    if (aplTo.element.type == "Program") {
-        return pairMatch(match, aplTo.children[0]);
+export class Matcher {
+    public matches: TreeNode<PairedNodes>[];
+    private internals: InternalDSLVariable;
+    private aplToFull: t.Node;
+    constructor(internals: InternalDSLVariable, aplToFull: t.Node) {
+        this.matches = [];
+        this.internals = internals;
+        this.aplToFull = aplToFull;
     }
-    try {
-        let node: TreeNode<PairedNodes> = new TreeNode(null, {
-            codeNode: match.element,
+
+    singleExprMatcher(
+        code: TreeNode<t.Node>,
+        aplTo: TreeNode<t.Node>
+    ): TreeNode<PairedNodes> | undefined {
+        let curMatches = this.checkCodeNode(code.element, aplTo.element);
+        curMatches =
+            curMatches && code.children.length >= aplTo.children.length;
+        // Current does not match and we have searched all the children, so just return early
+        // This ensures we only store the child search when we don't only have a partial match
+        let temp = [];
+        // If any matches bubble up from child nodes, we have to store it
+        for (let code_child of code.children) {
+            let maybeChildMatch = this.singleExprMatcher(code_child, aplTo);
+            if (maybeChildMatch) {
+                temp.push(maybeChildMatch);
+            }
+        }
+
+        // Filter the output for full matches :)
+        this.matches.push(
+            ...temp.filter((x) => x.element.aplToNode === this.aplToFull)
+        );
+        if (!curMatches) {
+            return;
+        }
+        // At this point current does match
+        let pairedCurrent: TreeNode<PairedNodes> = new TreeNode(null, {
+            codeNode: code.element,
             aplToNode: aplTo.element,
         });
-
         for (let i = 0; i < aplTo.children.length; i++) {
-            let child = pairMatch(match.children[i], aplTo.children[i]);
-            child.parent = node;
-            node.children.push(child);
+            let childSearch = this.singleExprMatcher(
+                code.children[i],
+                aplTo.children[i]
+            );
+            if (childSearch === undefined) {
+                // Failed to get a full match, so break here
+                return;
+            }
+            childSearch.parent = pairedCurrent;
+            pairedCurrent.children.push(childSearch);
         }
 
-        return node;
-    } catch (exception) {
-        return null;
-    }
-}
-
-function checkCodeNode(code_node: t.Node, aplTo: t.Node): boolean {
-    if (code_node.type != aplTo.type) {
-        return false;
+        // If we are here, a full match has been found
+        return pairedCurrent;
     }
 
-    //If not an internal DSL variable, gotta verify that the identifier is the same
-    if (code_node.type === "Identifier" && aplTo.type === "Identifier") {
-        if (code_node.name != aplTo.name) {
+    // This is broken
+    multiStatementMatcher(code: TreeNode<t.Node>, aplTo: TreeNode<t.Node>) {
+        console.log("Currently unsupported");
+    }
+
+    match(code: TreeNode<t.Node>, aplTo: TreeNode<t.Node>) {}
+    private checkCodeNode(code_node: t.Node, aplTo: t.Node): boolean {
+        // First verify the internal DSL variables
+
+        if (aplTo.type === "Identifier") {
+            if (aplTo.name in this.internals) {
+                if (this.internals[aplTo.name].includes(code_node.type)) {
+                    return true;
+                }
+
+                if (this.internals[aplTo.name].includes("Expression")) {
+                    return t.isExpression(code_node);
+                }
+            }
+        }
+
+        if (code_node.type != aplTo.type) {
             return false;
         }
-    }
-    for (let key of Object.keys(aplTo)) {
-        if (key in keys_to_ignore) {
-            continue;
+
+        //If not an internal DSL variable, gotta verify that the identifier is the same
+        if (code_node.type === "Identifier" && aplTo.type === "Identifier") {
+            if (code_node.name != aplTo.name) {
+                return false;
+            }
+        }
+        for (let key of Object.keys(aplTo)) {
+            if (key in keys_to_ignore) {
+                continue;
+            }
+
+            if (!Object.keys(code_node).includes(key)) {
+                return false;
+            }
         }
 
-        if (!Object.keys(code_node).includes(key)) {
-            return false;
-        }
+        return true;
     }
 
-    return true;
+    private buildPairTree(
+        code: TreeNode<t.Node>,
+        aplTo: TreeNode<t.Node>
+    ): TreeNode<PairedNodes> {
+        let temp: TreeNode<PairedNodes> = new TreeNode(null, {
+            codeNode: code.element,
+            aplToNode: aplTo.element,
+        });
+        if (code.children.length >= aplTo.children.length) {
+            for (let i = 0; i < aplTo.children.length; i++) {
+                let child = this.buildPairTree(
+                    code.children[i],
+                    aplTo.children[i]
+                );
+                temp.children.push(child);
+            }
+        } else {
+            console.log("ERROR");
+        }
+
+        return temp;
+    }
 }
