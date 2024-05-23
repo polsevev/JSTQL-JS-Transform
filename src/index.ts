dc04b92ef264aa67b948602d77833137d826d446
polsevev <rolf.martin@glomsrud.no>
2024-05-23 13:46:40 +0200

    optimized transformer and created evaluation

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,65 @@ import {
     TransformRecipe,
     transform,
 } from "./transform/transform";
-
+import { readdir } from "node:fs/promises";
 import { parseJSTQL } from "./langium/langiumRunner";
 
 const dir = "../prettier/src";
 
-const path = "test_files/test2.js";
+const path = "test_files/pipeline_test.js";
 const file = Bun.file(path);
 const codeFromFile = await file.text();
 const main = async () => {
     //transform(selfHostedTransformExampleMultiStmt, codeFromFile);
+    /*
     console.log(codeFromFile);
     const jstql_file =
-        "/home/rolfmg/Coding/Master/didactic-chainsaw/dsl_files/awaitToPromise.jstql";
+        "/home/rolfmg/Coding/Master/didactic-chainsaw/dsl_files/pipeline.jstql";
     const test_file = Bun.file(jstql_file);
     const test_JSTQL = await test_file.text();
     let proposals = await parseJSTQL(test_JSTQL);
 
-    let code = transform(proposals[0].cases, codeFromFile);
-    await Bun.write("output_files/test2.js", code);
+    let [code, count] = transform(proposals[0].cases, codeFromFile);
+    await Bun.write("output_files/pipeline_out.js", code);
+    return;
+    */
+    const jstql_file =
+        "/home/rolfmg/Coding/Master/didactic-chainsaw/dsl_files/pipeline.jstql";
+    const test_file = Bun.file(jstql_file);
+    const test_JSTQL = await test_file.text();
+    let proposals = await parseJSTQL(test_JSTQL);
+
+    let basepathExamplesJSFiles = "../next.js";
+    let examples = (await readdir(basepathExamplesJSFiles, { recursive: true }))
+        .filter((x) => x.endsWith(".js"))
+        .map((x) => basepathExamplesJSFiles + "/" + x);
+    console.log(examples);
+
+    let sum = 0;
+    console.log("Scripts found ", sum, "matches!");
+    let count = 0;
+    for (let examplesFile of examples) {
+        try {
+            if (examplesFile.split("/").includes("compiled")) {
+                continue;
+            }
+            console.log(examplesFile);
+            let script = await Bun.file(examplesFile).text();
+            let [resultString, matches] = transform(proposals[0].cases, script);
+            sum += matches;
+            if (matches > 0) {
+                await Bun.write(
+                    "output_testing/" + count + examplesFile.split("/").at(-1),
+                    resultString
+                );
+                count += 1;
+            }
+        } catch (e) {
+            console.log("Failed");
+        }
+        console.log("current sum", sum);
+    }
+    console.log(sum);
 };
 
 main();
--- src/transform/transformMatch.ts
+++ src/transform/transformMatch.ts
@@ -12,78 +12,146 @@ import { Match, MatchedTreeNode, PairedNodes } from "../matcher/matcher";
 import traverse from "@babel/traverse";
 import generate from "@babel/generator";
 import { TransformRecipe } from "./transform";
+import { Wildcard } from "../parser/parse";
 
 export function transformer(
     matches: Match[],
     transformTo: TreeNode<t.Node>,
     codeAST: t.Node,
-    traToAST: t.File
+    traToAST: t.File,
+    wildcardIdentifiers: string[]
 ): t.Node {
+    let transformedTransformTo: [Match, t.File][] = [];
     for (let match of matches.reverse()) {
         try {
             let traToWithWildcards = structuredClone(traToAST);
             for (let match_stmt of match.statements) {
+                let aplToWildcards: Map<string, t.Node[]> = new Map();
+                FindWildcardsAplTo(
+                    match_stmt,
+                    wildcardIdentifiers,
+                    aplToWildcards
+                );
                 transformMatch(match_stmt, transformTo, traToWithWildcards);
             }
-            traverse(codeAST, {
-                enter(path) {
-                    if (
-                        !(
-                            path.node.type === "Program" ||
-                            path.node.type === "File"
-                        )
-                    ) {
-                        if (
-                            path.node ===
-                            match.statements[0].element.codeNode[0]
-                        ) {
-                            path.replaceWithMultiple(
-                                traToWithWildcards.program.body
-                            );
-                            let siblings = path.getAllNextSiblings();
 
-                            // For multi line applicable to
-                            for (
-                                let i = 0;
-                                i < match.statements.length - 1;
-                                i++
-                            ) {
-                                siblings[i].remove();
-                            }
+            transformedTransformTo.push([match, traToWithWildcards]);
+        } catch (e) {
+            console.log(e);
+        }
+        //console.log("Finished replacement");
+    }
+    for (let [match, traToWithWildcards] of transformedTransformTo) {
+        traverse(codeAST, {
+            enter(path) {
+                if (
+                    !(path.node.type === "Program" || path.node.type === "File")
+                ) {
+                    if (path.node === match.statements[0].element.codeNode[0]) {
+                        path.replaceWithMultiple(
+                            traToWithWildcards.program.body
+                        );
+                        let siblings = path.getAllNextSiblings();
 
-                            // For when we have matched with *
+                        // For multi line applicable to
+                        for (let i = 0; i < match.statements.length - 1; i++) {
+                            siblings[i].remove();
+                        }
 
-                            for (let matchStmt of match.statements) {
-                                for (let [
-                                    i,
-                                    stmtMatchedWithStar,
-                                ] of matchStmt.element.codeNode.entries()) {
-                                    let siblingnodes = siblings.map(
-                                        (a) => a.node
-                                    );
-                                    if (
-                                        siblingnodes.includes(
+                        // For when we have matched with +
+
+                        for (let matchStmt of match.statements) {
+                            for (let [
+                                i,
+                                stmtMatchedWithStar,
+                            ] of matchStmt.element.codeNode.entries()) {
+                                let siblingnodes = siblings.map((a) => a.node);
+                                if (
+                                    siblingnodes.includes(stmtMatchedWithStar)
+                                ) {
+                                    let index =
+                                        siblingnodes.indexOf(
                                             stmtMatchedWithStar
-                                        )
-                                    ) {
-                                        let index =
-                                            siblingnodes.indexOf(
-                                                stmtMatchedWithStar
-                                            );
-                                        siblings[index].remove();
-                                    }
+                                        );
+                                    siblings[index].remove();
                                 }
                             }
                         }
                     }
-                },
-            });
-        } catch (e) {
-            console.log(e);
-        }
+                }
+            },
+        });
+    }
+    return codeAST;
+}
+
+function FindWildcardsAplTo(
+    match: TreeNode<PairedNodes>,
+    wildcardIdentifiers: string[],
+    out: Map<string, t.Node[]>
+) {
+    let ident = isWildcardIdent(wildcardIdentifiers, match.element.aplToNode);
+    if (!ident) {
+        return;
     }
 
-    return codeAST;
+    out.set(ident, match.element.codeNode);
+
+    for (let child of match.children) {
+        FindWildcardsAplTo(child, wildcardIdentifiers, out);
+    }
+}
+
+function isWildcardIdent(
+    wildcards: string[],
+    aplToNode: t.Node
+): string | undefined {
+    if (aplToNode.type === "Identifier") {
+        for (let ident of wildcards) {
+            if (aplToNode.name === ident) {
+                return ident;
+            }
+        }
+    } else if (
+        aplToNode.type === "ExpressionStatement" &&
+        aplToNode.expression.type === "Identifier"
+    ) {
+        for (let ident of wildcards) {
+            if (aplToNode.expression.name === ident) {
+                return ident;
+            }
+        }
+    }
+    return undefined;
+}
+
+export function transformTransformTo(
+    wildcardsMatchedWith: Map<string, t.Node>,
+    output: t.Node
+) {
+    traverse(output, {
+        Identifier: (path) => {
+            if (wildcardsMatchedWith.has(path.node.name)) {
+                let newNode = wildcardsMatchedWith.get(path.node.name);
+                if (newNode) {
+                    path.replaceWithMultiple(newNode);
+                }
+            }
+        },
+        ExpressionStatement: (path) => {
+            if (
+                path.node.expression.type === "Identifier" &&
+                wildcardsMatchedWith.has(path.node.expression.name)
+            ) {
+                let newNode = wildcardsMatchedWith.get(
+                    path.node.expression.name
+                );
+                if (newNode) {
+                    path.replaceWithMultiple(newNode);
+                }
+            }
+        },
+    });
 }
 
 export function transformMatch(
